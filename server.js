bdc0e43c3406939dfae3fe141e719f22f473027a
Fathy-Said-Gonaim <fsaaed@trenddc.com>
2026-02-24 01:50:18 +0200

    change env to deploy in render.com

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import { createServer } from "json-server";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+const jsonServer = createServer();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+app.use("/api", middlewares, router);
+
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
